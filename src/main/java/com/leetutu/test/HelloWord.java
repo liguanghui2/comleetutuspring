b2fe6a77b7b70d3bb2e7b797a63f596d1fbf868c
liguanghui1_yhd <liguanghui1@jd.com>
2017-12-21 22:19:06 +0800

    修改文件内容测试提交

--- src/main/java/com/leetutu/test/HelloWord.java
+++ src/main/java/com/leetutu/test/HelloWord.java
@@ -3,5 +3,6 @@ package com.leetutu.test;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("Hello Word again");
     }
 }
